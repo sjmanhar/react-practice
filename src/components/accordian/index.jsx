66d8f3e70212d4bc02a719ff9b3a740d2045ec89
sjmanhar <128324219+sjmanhar@users.noreply.github.com>
2024-08-28 04:50:16 +0530

    first commit

---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import data from './data';
+import '../accordian/styles.css';
+
+//single selection
+//multiple selection
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(getCurrentId){
+        console.log(getCurrentId)
+        setSelected(getCurrentId === selected ? null : getCurrentId)
+    }
+
+    console.log(selected)
+  return (
+    <div className='wrapper'>
+        <button>Enable Multiple Selection</button>
+        <div className='accordian'>
+            {
+                data && data.length > 0 ? 
+                data.map(dataItem=> <div className='item'>
+                    <div onClick={()=>handleSingleSelection(dataItem.id)} className='title'>
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                    {
+                        selected ===dataItem.id ?
+                        <div className='content'>{dataItem.answer}</div>
+                        : null
+                    }
+                </div>)
+                : <div>no data found</div>
+            }
+        </div>
+    </div>
+  )
+}
